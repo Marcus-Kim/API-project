cf4bec8b0a07a5c4d25ec398a4c12295bbd3dcf8
Marcus Kim <kim.marcus97@gmail.com>
2022-12-17 17:18:19 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,8 @@ const express = require('express')
   In this case, we are using it to initiate?? instance?? a router
 */
 
+const { check } = require('express-validator'); // from express-validator
+const { handleValidationErrors } = require('../../utils/validation'); // You made this
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 // Set token cookie is coming from auth.js in (../utils)
@@ -11,9 +13,40 @@ const { User } = require('../../db/models');
 // We are importing the User model in order to access the User.signup() declared in user. Thats it.
 const router = express.Router();
 
+/*
+For validateSignup:
+  - Why are these functions inside of an array?
+  - Is it necessary to have them in an array?
+
+  - Do we have them in this format for reusability? Plugging in to different route methods?
+  - Are these functions being invoked upon delcaration or is it waiting until the entire array is being passed in as middleware
+  - Do they invoke in order, same time, or is it random
+*/
+
+const validateSignup = [ //? Is this another error handling 'module??'
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors // how does this work being passed in as the last element
+];
+
 // Sign up
 router.post(
   '/',
+  validateSignup, //* here we are passing in the array of error-handling middleware
   async (req, res) => { // been seeing lots of async
     const { email, password, username } = req.body; // Taking the user input and storing them into variables
     // I think order of email, password, and username matters depending on how req.body is formatted
