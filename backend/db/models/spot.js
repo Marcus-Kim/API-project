1ba965ed5d8feb7d41b92c3fd33e1b2e680b50a8
Marcus Kim <kim.marcus97@gmail.com>
2022-12-21 12:20:20 -0700

    Added SpotImages schema and seeder

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Spot.belongsTo(models.User, { foreignKey: 'ownerId' })
       Spot.hasMany(models.Booking)
+      Spot.hasMany(models.SpotImage)
     }
   }
   Spot.init({
