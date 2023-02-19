ac30470fdf4f93fb48ae52ad76483d3f8f603a73
Marcus Kim <kim.marcus97@gmail.com>
2023-02-18 17:15:45 -0700

    Sorted reviews from newest to oldest

--- frontend/src/components/Reviews/SpotDetailsReviews/index.js
+++ frontend/src/components/Reviews/SpotDetailsReviews/index.js
@@ -11,7 +11,7 @@ function SpotDetailsReviews({ spotId }) {
 
   const reviews = useSelector(state => Object.values(state.reviews.spot))
   const spot = useSelector(state => state.spots.singleSpot)
-  // const session = useSelector(state => console.log(state.session.user.id))
+  const userId = useSelector(state => state.session.user.id)
 
   useEffect(() => {
     if (spotId) {
@@ -25,22 +25,23 @@ function SpotDetailsReviews({ spotId }) {
 
   let rating = spot.avgStarRating;
 
-  console.log(reviews)
+  const sortedReviews = reviews.reverse()
+
   return (
     <div className="spot-details-reviews-container">
       <div className="spot-details-reviews-header-container">
-        <div className="spot-details-reviews-rating-count"><i className="fa-solid fa-star"></i> {!spot.numReviews ? `New` : `${parseFloat(spot.avgStarRating).toFixed(1)} • ${spot.numReviews} Reviews`} </div>
+        <div className="spot-details-reviews-rating-count"><i className="fa-solid fa-star"></i> {!spot.numReviews ? `New` : `${parseFloat(spot.avgStarRating).toFixed(1)} • ${spot.numReviews} ${(spot.numReviews === 1 ? "Review" : "Reviews")}`} </div>
         <div className="post-review-button-container">
           <OpenModalButton className="post-review-button" modalComponent={<PostReviewModal spotId={spot.id} />} buttonText="Post Your Review" />
         </div>
       </div>
-      {reviews.length === 0 ? <div>Be the first to post a review!</div> : reviews.map(review => (
+      {reviews.length === 0 ? <div>Be the first to post a review!</div> : sortedReviews.map(review => (
         <div key={review.review} className="spot-details-reviews-list-wrapper">
           <div className="spot-details-reviews-firstName">{review.User && review.User.firstName}</div>
           <div className="spot-details-reviews-date">{review.updatedAt.slice(0, 10)}</div>
           <div className="spot-details-reviews-text">{review.review}</div>
           <div className="spot-details-reviews-delete-button">
-            <OpenModalButton modalComponent={<DeleteReviewModal reviewId={review.id} spotId={spotId}/>} buttonText="Delete" />
+            {userId == review.userId && <OpenModalButton modalComponent={<DeleteReviewModal reviewId={review.id} spotId={spotId}/>} buttonText="Delete" />}
           </div>
         </div>
       ))}
